bc0e53830235d9fccd1cbabfaf7dae3e1d6dac21
smgA113 <kaloian4o05@gmail.com>
2023-10-24 15:39:54 +0300

    Initial commit

---
+++ sql_scripts/20231024_insert_test_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `category`(`name`,`created`,`is_active`) VALUES ('Milk', '2023-10-14 10:50:00', 1);
+INSERT INTO `category`(`name`,`created`,`is_active`) VALUES ('Meat', '2023-10-14 10:50:00', 1);
+INSERT INTO `category`(`name`,`created`,`is_active`) VALUES ('Vegetables', '2023-10-14 10:50:00', 1);
+INSERT INTO `category`(`name`,`created`,`is_active`) VALUES ('Fruits', '2023-10-14 10:50:00', 0);
+INSERT INTO `store`(`name`,`city_id`,`address`) VALUES ('Avanti', '2', 'bul. Bulgaria 90');
+INSERT INTO `store`(`name`,`city_id`,`address`) VALUES ('Fantastiko', '2', 'ul. Alabin 23');
+INSERT INTO `store`(`name`,`city_id`,`address`) VALUES ('Fantastiko', '1', 'bul. Demokraciq 16');
+INSERT INTO `store`(`name`,`city_id`,`address`) VALUES ('Avanti', '2', 'ul. Iskar 61');
